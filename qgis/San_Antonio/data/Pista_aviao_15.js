5b0748556dec4470f7f86c2b7c1089e9575edf04
FazendasBartira <dgsh@fazendasbartira.com.br>
2023-06-09 11:42:30 -0300

    San_Antonio

--- qgis/San_Antonio/data/Pista_aviao_15.js
+++
@@ -1,1 +0,0 @@
-var json_Pista_aviao_15 = {"type":"FeatureCollection","name":"Pista_aviao_15","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1"},"geometry":{"type":"Point","coordinates":[-57.920186214473226,-14.354020076535598]}}]}
