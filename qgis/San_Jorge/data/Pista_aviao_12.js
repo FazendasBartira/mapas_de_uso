6b743410021a1cfc75ab4c1625c0d634c20b02af
FazendasBartira <dgsh@fazendasbartira.com.br>
2023-06-23 13:46:44 -0300

    Alteracao_San_jorge_23_06_2023

---
+++ qgis/San_Jorge/data/Pista_aviao_12.js
@@ -0,0 +1,1 @@
+var json_Pista_aviao_12 = {"type":"FeatureCollection","name":"Pista_aviao_12","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1"},"geometry":{"type":"Point","coordinates":[-53.179809518899312,-18.719547443874301]}}]}
